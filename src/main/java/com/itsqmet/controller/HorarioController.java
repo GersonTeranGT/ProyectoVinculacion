688bdd476d7ebd80d0f50f6604c6e7dcfad06efb
Mick-dud <mba.arevalosabando@gmail.com>
2026-01-25 19:21:08 -0500

    cambios

--- src/main/java/com/itsqmet/controller/HorarioController.java
+++ src/main/java/com/itsqmet/controller/HorarioController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HorarioController {
     @GetMapping
     public String registrarHorario(){
-        return "pages/registroHorario";
+        return "pages/crearHorarioBachillerato";
     }
 }
